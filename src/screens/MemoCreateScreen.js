f284511f318c74c616406c18b7670beac4d4e7f7
mas <mas@mas-no-MacBook.local>
2020-04-15 17:47:41 +0900

    Settings for release

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 import firebase from 'firebase';
 import 'firebase/firestore';
 import CircleButton from '../elements/CircleButton';
@@ -28,7 +28,7 @@ class MemoCreateScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -37,7 +37,7 @@ class MemoCreateScreen extends React.Component {
           textAlignVertical="top"
         />
         <CircleButton name="check" onPress={this.handlePress.bind(this)} />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 import firebase from 'firebase';
 import CircleButton from '../elements/CircleButton';
 
@@ -45,7 +45,7 @@ class MemoEditScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -55,7 +55,7 @@ class MemoEditScreen extends React.Component {
           textAlignVertical="top"
         />
         <CircleButton name="check" onPress={this.handlePress.bind(this)} />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
